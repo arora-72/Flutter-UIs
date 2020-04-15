1ab58d7360a0bc061aea64c0979633159d6319ee
arora-72 <indresh.arora2016@vitstudent.ac.in>
2020-04-16 03:31:53 +0530

    added home top widget

---
+++ streaming_service_ui/lib/screens/home_page_screen.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+import 'package:streaming_service_ui/widgets/home_screen_top_widget.dart';
+
+class HomePageScreen extends StatefulWidget {
+  @override
+  _HomePageScreenState createState() => _HomePageScreenState();
+}
+
+class _HomePageScreenState extends State<HomePageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            HomeScreeTopPart(),
+          ],
+        ),
+      ),
+      
+    );
+  }
+}
